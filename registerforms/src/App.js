0ee03cd9f3739daf0010abb41ca9117cd4df665a
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-11-30 11:06:11 +0200

    signup

---
+++ registerforms/src/App.js
@@ -0,0 +1,15 @@
+
+import Header from "./components/header";
+import RegistrationForm from "./components/registrationForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
